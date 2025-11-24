d9f7c9f2c476df74b1756b26b7a9919611076acd
Stefano Taillefert <stefano.taille@gmail.com>
2025-11-24 23:05:21 +0100

    Fix pipeline

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const URI = process.env.MONGODB_URI;
 
 if (!URI) {
-  throw new Error("Please define MONGODB_URI in environment variables");
+  console.error("Please define MONGODB_URI in environment variables");
 }
 
 interface Cached {
